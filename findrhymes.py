8129f0dd3f5fd5e4b81afe494bf0750cd8d50856
Harper Pollio-Barbee <hpolliobarbee@brandeis.edu>
2019-12-21 11:26:13 -0500

    Started featural rhymes module as class, commented more code

--- findrhymes.py
+++ findrhymes.py
@@ -1,6 +1,7 @@
 import nltk
 import nltk.corpus
 
+# finds the part of the word that makes it "rhyme"
 def findrhyme(phones):
     for i in range(len(phones)):
         if len(phones[-i]) == 3:  # checks to see if the phone is 3 characters long (only vowels are 3 long)
@@ -9,8 +10,22 @@ def findrhyme(phones):
 
 
 entries = nltk.corpus.cmudict.entries()
-vowels = {'AA', 'AE', 'AH', 'AO', 'AW', 'AY', 'EH', 'ER', 'EY', 'IH', 'IY', 'OW', 'OY', 'UH', 'UW'}
+
+# all vowels with no stress for featural rhymes
+vowels0 = {'AA0', 'AE0', 'AH0', 'AO0', 'AW0', 'AY0', 'EH0', 'ER0', 'EY0', 'IH0', 'IY0', 'OW0', 'OY0', 'UH0', 'UW0'}
+# all vowels with primary stress for featural rhymes
+vowels1 = {'AA1', 'AE1', 'AH1', 'AO1', 'AW1', 'AY1', 'EH1', 'ER1', 'EY1', 'IH1', 'IY1', 'OW1', 'OY1', 'UH1', 'UW1'}
+# all vowels with secondary stess  for featural rhymes
+vowels2 = {'AA2', 'AE2', 'AH2', 'AO2', 'AW2', 'AY2', 'EH2', 'ER2', 'EY2', 'IH2', 'IY2', 'OW2', 'OY2', 'UH2', 'UW2'}
+
+# dictionary containing every consonant as a key with the entry being the two closest phones to the key
+ranking = {'B': ['P', 'D'], 'CH': ['SH', 'JH'], 'D': ['D','JH'], 'DH': ['TH', 'V'], 'F': ['V', 'TH'], 'G': ['K', 'D'],
+           'JH': ['ZH', 'CH'], 'K': ['G', 'T'], 'L': 'R', 'M': ['N', 'NG'], 'N': ['NG', 'M'], 'NG': ['N', 'NG'],
+           'P': ['B', 'T'], 'R': 'L', 'S': ['Z', 'T'], 'SH': ['ZH', 'CH'], 'T': ['D', 'CH'], 'TH': ['DH', 'F'],
+           'V': ['F', 'DH'], 'Z': ['S', 'ZH'], 'ZH': ['JH', 'SH']}
+
 rhymedict = {}
+
 for (wrd, pron) in entries:
     rhyme = findrhyme(pron)  # gets the rhyme of the word in an entry in the CMUdict
     if rhyme in rhymedict:
@@ -40,6 +55,20 @@ def subseqrhyme(word):
             if entry in rhymedict:
                 rhymelist.append(rhymedict[entry])
         return rhymelist
+
+
+# class responsible for getting all featural rhymes from rhyme
+class FeatureRhyme:
+    def __init__(self, rnkng, rhm):
+        self.ranking = rnkng
+        self.rhm = rhm
+        self.rhmconcount = len(rhm)
+
+    def findconsonant(self, rhm):
+        for phone in rhm:
+            if phone in vowels0 or phone in vowels1 or phone in vowels2:
+                self.rhmconcount -= 1
+
 
 def searchrhyme(word):
     if word in nltk.corpus.cmudict.words():
